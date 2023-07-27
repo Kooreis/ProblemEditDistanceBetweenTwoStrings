96c44f35844073ea29d14fb56536b0a93c88f3f9
Kooreis <kooreis.official@gmail.com>
2023-07-26 21:14:39 -0500

    In this update, the main method is created. It prompts the user to enter two strings and reads them using the Scanner class.

--- solution.java
+++ solution.java
@@ -1,6 +1,3 @@
-Here is a Java console application that calculates the edit distance between two strings using the dynamic programming approach.
-
-```java
 import java.util.Scanner;
 
 public class Main {
@@ -10,31 +7,5 @@ public class Main {
         String str1 = scanner.nextLine();
         System.out.println("Enter the second string:");
         String str2 = scanner.nextLine();
-        System.out.println("The edit distance between the two strings is: " + editDistance(str1, str2));
-    }
-
-    public static int editDistance(String str1, String str2) {
-        int len1 = str1.length();
-        int len2 = str2.length();
-        int[][] dp = new int[len1 + 1][len2 + 1];
-
-        for (int i = 0; i <= len1; i++) {
-            for (int j = 0; j <= len2; j++) {
-                if (i == 0) {
-                    dp[i][j] = j;
-                } else if (j == 0) {
-                    dp[i][j] = i;
-                } else if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = 1 + Math.min(dp[i][j - 1], Math.min(dp[i - 1][j], dp[i - 1][j - 1]));
-                }
-            }
-        }
-
-        return dp[len1][len2];
     }
 }
-```
-
-This program first takes two strings as input from the user. It then calculates the edit distance between these two strings using a dynamic programming approach. The edit distance is the minimum number of operations (insertions, deletions, and substitutions) required to transform one string into another. The result is then printed to the console.
