fcc52fecc1653e218b5f32301474b650248c9b5d
Kooreis <kooreis.official@gmail.com>
2023-07-26 21:14:39 -0500

    Initial setup of the edit_distance function. This function takes two strings and their lengths as input. If the length of either string is 0, it returns the length of the other string.

--- solution.py
+++ solution.py
@@ -5,20 +5,3 @@ def edit_distance(str1, str2, m, n):
 
     if n == 0:
         return m
-
-    if str1[m-1] == str2[n-1]:
-        return edit_distance(str1, str2, m-1, n-1)
-
-    return 1 + min(edit_distance(str1, str2, m, n-1),    # Insert
-                   edit_distance(str1, str2, m-1, n),    # Remove
-                   edit_distance(str1, str2, m-1, n-1)    # Replace
-                   )
-
-def main():
-    str1 = input("Enter the first string: ")
-    str2 = input("Enter the second string: ")
-
-    print(edit_distance(str1, str2, len(str1), len(str2)))
-
-if __name__ == "__main__":
-    main()
