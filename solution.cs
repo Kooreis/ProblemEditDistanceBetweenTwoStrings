87a33bada4204dab6b95c4c2b5fda75b89083a40
Kooreis <kooreis.official@gmail.com>
2023-07-27 21:14:39 -0500

    Added user input for two strings.

--- solution.cs
+++ solution.cs
@@ -4,5 +4,10 @@ class Program
 {
     static void Main()
     {
+        Console.Write("Enter the first string: ");
+        string str1 = Console.ReadLine();
+
+        Console.Write("Enter the second string: ");
+        string str2 = Console.ReadLine();
     }
 }
