43cc71aa4b3caaf443db2c92fa4acea21f19cd3c
Kooreis <kooreis.official@gmail.com>
2023-07-27 21:14:39 -0500

    In this update, the editDistance method is created. It calculates the length of the two input strings and initializes a 2D array to store the intermediate results of the dynamic programming approach.

--- solution.java
+++ solution.java
@@ -1,11 +1,5 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the first string:");
-        String str1 = scanner.nextLine();
-        System.out.println("Enter the second string:");
-        String str2 = scanner.nextLine();
-    }
+public static int editDistance(String str1, String str2) {
+        int len1 = str1.length();
+        int len2 = str2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
 }
